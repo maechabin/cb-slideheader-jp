c3a916cf37dc681915ac975108d25193e727a0b7
maechabin <mitoppo@gmail.com>
2018-07-04 22:39:56 +0900

    不要なファイルを削除（#5）

--- src/test.js
+++
@@ -1,8 +0,0 @@
-import { SlideHeader } from '../dist/slideheader.min';
-
-window.onload = () => {
-  new SlideHeader('.cb-header', {
-    isHeadroom: true,
-    slidePoint: 64,
-  }).init('slideUp');
-};
